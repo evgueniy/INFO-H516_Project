09c4c7db7a5c14644a569680d7aa60b422762079
evgueniy <evgueniy.st@gmail.com>
2025-05-06 02:22:34 +0200

    script update for new file format and arg

--- Script/plot_psnr.py
+++ Script/plot_psnr.py
@@ -52,13 +52,30 @@ def plotDataFrame(dataFrame, intraPeriod: list):
     plt.show()
 
 argSize = len(sys.argv)
-if (argSize > 3 and argSize < 6) or argSize < 3:
-    print("too few arg, either file + 4 qp's or file + all qp's between 2 and 48")
+if argSize not in (3, 7):
+    print("too few arg, either file + encoding or file + 4 qp's + encoding (all qp's between 2 and 48)")
     sys.exit(1)
 
 yuvFile = sys.argv[1]
 if not os.path.exists(yuvFile):
     print(f"Error: file {yuvFile} does not exist.")
+    sys.exit(1)
+
+if argSize == 7:
+    qps = [int(sys.argv[i]) for i in range(2,6)]
+    encodingArg = sys.argv[6]
+elif argSize == 3:
+    qps = [i for i in range(2,49)]
+    encodingArg = sys.argv[2]
+
+if encodingArg.lower() == "original":
+    enc_suffix = "0"
+elif encodingArg.lower() == "cabac":
+    enc_suffix = "1"
+elif encodingArg.lower() == "huffman":
+    enc_suffix = "2"
+else:
+    print("Invalid encoding option. Must be one of original, cabac, huffman")
     sys.exit(1)
 
 name = os.path.basename(yuvFile)
@@ -67,27 +84,27 @@ print("Before change:", os.getcwd())
 os.chdir("../ICSPCodec/build/Release/")
 print("After change:", os.getcwd())
 
-qps = [int(sys.argv[i]) for i in range(2,6)] if argSize > 3 else [i for i in range(2,49)]
+print(f"File: {yuvFile}\n--> Qp's: {qps} for inter values: [0,8,16,32,300]")
+yuvFile = f'../../data/{os.path.basename(yuvFile)}'
+print(f"File after change: {yuvFile}\n--> Qp's: {qps} for inter values: [0,8,16,32,300]")
+
 intraPeriod = [0,8,16,32,300]
 bitrate = {f'bitrate_{key}' : [] for key in intraPeriod}
 psnrs = {f'psnr_{key}': [] for key in intraPeriod}
-print(f"File: {yuvFile}\n--> Qp's: {qps} for inter values: {intraPeriod}")
-yuvFile = f'../../data/{os.path.basename(yuvFile)}'
-print(f"File after change: {yuvFile}\n--> Qp's: {qps} for inter values: {intraPeriod}")
 
 for i in range(len(qps)):
     for j in range(len(intraPeriod)):
-        csvFilename = f"../../results/{name}_{qps[i]}_{qps[i]}_{intraPeriod[j]}.csv"
+        csvFilename = f"../../results/{name}_{qps[i]}_{qps[i]}_{intraPeriod[j]}_{enc_suffix}.csv"
         if not os.path.exists(csvFilename):
-            print(f"Generating data for qp: {qps[i]} - inter: {intraPeriod[j]}")
-            commandArgs = [f"{os.getcwd()}/ICSPCodec", "-i", yuvFile, "-n", "300", "-q", f"{qps[i]}", "--intraPeriod", f"{intraPeriod[j]}", "--EnMultiThread", "0"]
+            print(f"Generating data for qp: {qps[i]} - inter: {intraPeriod[j]} - encoder: {encodingArg}")
+            commandArgs = [f"{os.getcwd()}/ICSPCodec", "-i", yuvFile, "-n", "300", "-q", f"{qps[i]}", "--intraPeriod", f"{intraPeriod[j]}", "--EnMultiThread", "0","-e",encodingArg]
             subprocess.run(commandArgs, capture_output=True, text=True, cwd=os.getcwd())
         else:
             print(f"Data for qp: {qps[i]} - inter: {intraPeriod[j]} exists skipping..")
 
 for qp in qps:
     for intra in intraPeriod:
-        csvFilename = f"../../results/{name}_{qp}_{qp}_{intra}.csv"
+        csvFilename = f"../../results/{name}_{qp}_{qp}_{intra}_{enc_suffix}.csv"
         print(f"reading: {csvFilename}")
         if not os.path.exists(csvFilename):
             print(f"Error: file {csvFilename} does not exist.")
@@ -95,7 +112,7 @@ for qp in qps:
         dataFrame = pd.read_csv(csvFilename, sep=";", decimal=".")
         psnr = dataFrame['AvgPSNR'].iloc[-1]
         print(f"avg psnr {psnr}")
-        binPath = f"../../results/{name}_compCIF_{qp}_{qp}_{intra}.bin"
+        binPath = f"../../results/{name}_compCIF_{qp}_{qp}_{intra}_{enc_suffix}.bin"
         try:
             size = os.path.getsize(binPath) * 8
         except Exception as e:
